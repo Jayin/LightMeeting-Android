0212cea1b6bc3c23262906b5ddb0aa8825f6248a
jason <zyt769562065@163.com>
2014-11-11 20:00:22 +0800

    xml更改为密码不可见 成功调用登录接口

--- src/meizhuo/org/lightmeeting/acty/Login.java
+++ src/meizhuo/org/lightmeeting/acty/Login.java
@@ -1,13 +1,24 @@
 package meizhuo.org.lightmeeting.acty;
 
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import meizhuo.org.lightmeeting.R;
+import meizhuo.org.lightmeeting.api.UserAPI;
 import meizhuo.org.lightmeeting.app.BaseActivity;
+import meizhuo.org.lightmeeting.imple.JsonResponseHandler;
+import meizhuo.org.lightmeeting.utils.AndroidUtils;
+import meizhuo.org.lightmeeting.utils.EditTextUtils;
+import meizhuo.org.lightmeeting.utils.StringUtils;
+import meizhuo.org.lightmeeting.widget.LoadingDialog;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.ViewFlipper;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * 登录注册页面
@@ -34,6 +45,7 @@ public class Login extends BaseActivity {
 	@InjectView(R.id.acty_login_et_password) EditText login_et_password;
 	@InjectView(R.id.acty_login_btn_login) Button login_btn_login;
 	@InjectView(R.id.acty_login_btn_regist) Button login_btn_regist;
+	LoadingDialog dialog;
 	
 	
 	
@@ -41,8 +53,81 @@ public class Login extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState,R.layout.acty_login);
+		initFlipper();
+		dialog = new LoadingDialog(this);
 	}
-
+	
+	//登录
+	@OnClick(R.id.acty_login_btn_login) public void login(){
+		if(!(AndroidUtils.isNetworkConnected(this)))
+		{
+			toast("请打开你的网络开关");
+			return ;
+		}
+		if(StringUtils.isEmpty(EditTextUtils.getText(login_et_username))||StringUtils.isEmpty(EditTextUtils.getText(login_et_password))){
+			toast("不能为空");
+			return ;
+		}
+		//用户登录
+		UserAPI.login(EditTextUtils.getText(login_et_username), EditTextUtils.getText(login_et_password), new JsonResponseHandler() {
+			
+			@Override
+			public void onStart() {
+				// TODO Auto-generated method stub
+				if (dialog == null)
+					dialog = new LoadingDialog(Login.this);
+				dialog.setText("正在登录");
+				dialog.show();
+			}
+			
+			@Override
+			public void onOK(Header[] headers, JSONObject obj) {
+				// TODO Auto-generated method stub
+				try {
+					if(obj.getString("code").equals("20000")){
+						if(dialog.isShowing())
+						{
+							dialog.dismiss();
+							dialog = null;
+						}
+						String message = obj.getString("response");
+						toast(message);
+						return ;
+					}
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					toast("出现了异常!");
+				}
+			}
+			
+			@Override
+			public void onFaild(int errorType, int errorCode) {
+				// TODO Auto-generated method stub
+				if(dialog.isShowing()){
+					dialog.dismiss();
+					dialog = null;
+				}
+				toast("网络不给力,请检查你的网络设置 ！");
+				
+			}
+		});
+	}
+	//去注册
+	@OnClick(R.id.acty_login_btn_regist) public void to_regist(){
+		
+	}
+	//点击就进行注册
+	@OnClick(R.id.acty_register_btn_regist) public void regist(){
+		
+	}
+	//取消注册
+	@OnClick(R.id.acty_register_btn_cancle) public void cacle(){
+		
+	}
+	
+	
+	
 	@Override
 	protected void initData() {
 		// TODO Auto-generated method stub
@@ -53,6 +138,11 @@ public class Login extends BaseActivity {
 	protected void initLayout() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private void initFlipper(){
+		flipper.setInAnimation(this, R.anim.push_up_in);
+		flipper.setOutAnimation(this, R.anim.push_up_out);
 	}
 
 }
