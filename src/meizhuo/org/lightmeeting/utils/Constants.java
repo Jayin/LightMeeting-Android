8ea3b834482d2eb22eb9454c2e825facf0c1092e
jason <zyt769562065@163.com>
2014-11-11 11:38:47 +0800

    一些Action常量跟其他常量

---
+++ src/meizhuo/org/lightmeeting/utils/Constants.java
@@ -0,0 +1,19 @@
+package meizhuo.org.lightmeeting.utils;
+
+/**
+ * 常量跟注册的Action都放这里
+ * @author Jason
+ *
+ */
+public class Constants {
+	
+	/**Action*/
+	/**去登陆*/
+	public static final String Action_To_Login = "To_Login";
+	/**用户登录成功*/
+	public static final String Action_Login_In_Successful = "Login_In_Successful";
+	/**用户登录失败*/
+	public static final String Action_Login_failed = "Login_failed";
+
+
+}
