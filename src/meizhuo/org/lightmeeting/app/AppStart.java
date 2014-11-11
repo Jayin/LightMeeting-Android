af196aa98c91ef6e4e5dcfd5b80cfe8cf89e60f3
jason <zyt769562065@163.com>
2014-11-11 10:59:46 +0800

    AppStart

---
+++ src/meizhuo/org/lightmeeting/app/AppStart.java
@@ -0,0 +1,16 @@
+package meizhuo.org.lightmeeting.app;
+
+import meizhuo.org.lightmeeting.R;
+import android.os.Bundle;
+
+public class AppStart extends BaseActivity{
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState,R.layout.acty_start);
+	}
+
+
+}
