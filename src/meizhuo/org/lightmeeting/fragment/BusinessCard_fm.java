3e6537d4c76eb618a43f382e0c142c9362d6ac59
jason <zyt769562065@163.com>
2014-11-14 14:48:55 +0800

    用户密码修改做判断

--- src/meizhuo/org/lightmeeting/fragment/BusinessCard_fm.java
+++ src/meizhuo/org/lightmeeting/fragment/BusinessCard_fm.java
@@ -15,12 +15,18 @@ import meizhuo.org.lightmeeting.model.User;
 import meizhuo.org.lightmeeting.utils.Constants;
 import meizhuo.org.lightmeeting.utils.L;
 import meizhuo.org.lightmeeting.widget.LoadingDialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class BusinessCard_fm extends BaseFragment  {
@@ -35,6 +41,13 @@ public class BusinessCard_fm extends BaseFragment  {
 	
 	
 	BCHandler handler = new BCHandler();
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -135,6 +148,111 @@ private void initLayout(){
 		}
 		
 	}
+	
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		// TODO Auto-generated method stub
+		inflater.inflate(R.menu.fm_user, menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.action_refreshdata:
+			toast("1");
+			break;
+		case R.id.action_changePsw:
+			LayoutInflater inflater = LayoutInflater.from(getActivity());
+			View dialogView = inflater.inflate(R.layout.dialog_change_psw, null);
+			final EditText et_change_oldpsw = (EditText)dialogView.findViewById(R.id.et_change_oldpsw);
+			final EditText et_change_newpsw = (EditText)dialogView.findViewById(R.id.et_change_newpsw);
+			final EditText et_confirm_newpsw = (EditText)dialogView.findViewById(R.id.et_change_confirmnewpsw);
+			AlertDialog.Builder  builder =  new AlertDialog.Builder(getActivity());
+			builder.setTitle("修改密码");
+			builder.setView(dialogView);
+			builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					if((et_change_oldpsw.getText().toString()) == null || (et_change_oldpsw.getText().toString()).equals(""))
+					{
+						toast("旧密码不能为空!");
+						return ;
+					}
+					if((et_change_newpsw.getText().toString()) == null || (et_change_newpsw.getText().toString()).equals(""))
+					{
+						toast("新密码不能为空!");
+						return ;
+					}
+					if((et_change_oldpsw.getText().toString()) == null || (et_change_oldpsw.getText().toString()).equals(""))
+					{
+						toast("旧密码不能为空!");
+						return ;
+					}
+					if(!((et_confirm_newpsw.getText().toString()).equals(et_change_newpsw.getText().toString()))){
+						toast("两次新密码输入不一致,修改失败!");
+						return ;
+					}
+					UserAPI.changePsw(et_change_oldpsw.getText().toString(), et_change_newpsw.getText().toString(), new JsonResponseHandler() {
+						
+						@Override
+						public void onOK(Header[] headers, JSONObject obj) {
+							// TODO Auto-generated method stub
+							
+							try {
+								if(obj.getString("code").equals("40000")){
+									String message = obj.getString("msg");
+									toast(message);
+									return ;
+								}
+							} catch (JSONException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							
+							try {
+								if(obj.getString("code").equals("20000"))
+								{
+									toast("修改成功");
+									
+								}
+							} catch (JSONException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						
+							
+						}
+						
+						@Override
+						public void onFaild(int errorType, int errorCode) {
+							// TODO Auto-generated method stub
+							
+						}
+					});
+					
+				}
+			});
+			builder.setNegativeButton("取消", null);
+			AlertDialog dialog = builder.create();
+			dialog.show();
+			
+			break;
+		case R.id.action_logoff:
+			toast("1");
+			break;
+		case R.id.action_logout:
+			toast("1");
+			break;
+		default:
+			break;
+		}
+		return true;
+	}
+	
 	
 	
 
