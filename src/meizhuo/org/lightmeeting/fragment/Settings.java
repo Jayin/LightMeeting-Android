9aea93447624303470d8d54fa340c8f47c74c068
jason <zyt769562065@163.com>
2014-11-17 17:14:38 +0800

    设置

---
+++ src/meizhuo/org/lightmeeting/fragment/Settings.java
@@ -0,0 +1,41 @@
+package meizhuo.org.lightmeeting.fragment;
+
+import butterknife.InjectView;
+import meizhuo.org.lightmeeting.R;
+import meizhuo.org.lightmeeting.utils.AndroidUtils;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Settings extends BaseFragment{
+	@InjectView(R.id.tv_versionCode) TextView tv_versionCode;
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreateView(inflater, container, savedInstanceState,R.layout.fragment_about);
+		String versionName;
+		try {
+			versionName = AndroidUtils.getAppVersionName(getActivity());
+			tv_versionCode.setText("版本" + versionName);
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return contentView;
+	}
+	@Override
+	protected void initData() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	protected void initLayout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
