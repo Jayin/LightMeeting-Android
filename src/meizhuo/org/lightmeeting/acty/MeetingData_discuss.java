01b2a626976f06917af48d9ce4dc1a15748cd5b3
jason <zyt769562065@163.com>
2014-11-17 17:15:24 +0800

    讨论页面

---
+++ src/meizhuo/org/lightmeeting/acty/MeetingData_discuss.java
@@ -0,0 +1,29 @@
+package meizhuo.org.lightmeeting.acty;
+
+import android.os.Bundle;
+import meizhuo.org.lightmeeting.app.BaseActivity;
+
+public class MeetingData_discuss extends BaseActivity {
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+
+	@Override
+	protected void initData() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initLayout() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
