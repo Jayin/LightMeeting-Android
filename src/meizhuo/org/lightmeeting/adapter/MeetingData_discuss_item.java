a2dd4b4b0026de5fe59ad83bf430c6dcbb143fe1
jason <zyt769562065@163.com>
2014-11-19 11:46:44 +0800

    评论api

---
+++ src/meizhuo/org/lightmeeting/adapter/MeetingData_discuss_item.java
@@ -0,0 +1,41 @@
+package meizhuo.org.lightmeeting.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class MeetingData_discuss_item extends BaseAdapter{
+	
+	private Context context;
+	private 
+	public MeetingData_discuss_item() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
