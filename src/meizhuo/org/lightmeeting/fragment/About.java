8736505f21de00564b035494981c346c69aaaf5c
Jayin Ton <tonjayin@gmail.com>
2014-11-22 10:47:55 +0800

    * 修改UI: 启动页，关于页 * 修改图标

--- src/meizhuo/org/lightmeeting/fragment/About.java
+++ src/meizhuo/org/lightmeeting/fragment/About.java
@@ -1,28 +1,32 @@
 package meizhuo.org.lightmeeting.fragment;
 
-import butterknife.InjectView;
 import meizhuo.org.lightmeeting.R;
 import meizhuo.org.lightmeeting.utils.AndroidUtils;
+import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class About extends BaseFragment {
-	
+
 	@InjectView(R.id.tv_versionCode) TextView tv_versionCode;
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	@InjectView(R.id.tv_homepage) TextView tv_homepage;
+
+	@Override public View onCreateView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		super.onCreateView(inflater, container, savedInstanceState,R.layout.fragment_about);
+		super.onCreateView(inflater, container, savedInstanceState,
+				R.layout.fragment_about);
 		String versionName;
 		try {
 			versionName = AndroidUtils.getAppVersionName(getActivity());
-			tv_versionCode.setText("版本" + versionName);
+			tv_versionCode.setText("版本 : " + versionName);
 		} catch (NameNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -30,17 +34,23 @@ public class About extends BaseFragment {
 		return contentView;
 	}
 
-	@Override
-	protected void initData() {
-		// TODO Auto-generated method stub
-		
+	@OnClick(R.id.tv_homepage)
+	public void homepage(){
+		Intent intent = new Intent();        
+        intent.setAction("android.intent.action.VIEW");    
+        Uri content_url = Uri.parse(tv_homepage.getText().toString());   
+        intent.setData(content_url);  
+        startActivity(intent);
 	}
 
-	@Override
-	protected void initLayout() {
+	@Override protected void initData() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override protected void initLayout() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
