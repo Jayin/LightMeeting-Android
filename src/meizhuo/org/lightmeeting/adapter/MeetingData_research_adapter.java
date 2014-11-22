d9ce2c3d202e4070c59f8f482fa3413942e8f227
jason <zyt769562065@163.com>
2014-11-22 19:07:02 +0800

    移除调查的删除和编辑功能

--- src/meizhuo/org/lightmeeting/adapter/MeetingData_research_adapter.java
+++ src/meizhuo/org/lightmeeting/adapter/MeetingData_research_adapter.java
@@ -2,26 +2,21 @@ package meizhuo.org.lightmeeting.adapter;
 
 import java.util.List;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 import meizhuo.org.lightmeeting.R;
-import meizhuo.org.lightmeeting.adapter.MeetingData_discuss_adapter.OnEditListener;
-import meizhuo.org.lightmeeting.adapter.MeetingData_discuss_adapter.OnHandleListener;
-import meizhuo.org.lightmeeting.adapter.MeetingData_discuss_adapter.OnItemClickListener;
-import meizhuo.org.lightmeeting.adapter.MeetingData_discuss_adapter.OnUpdateListener;
 import meizhuo.org.lightmeeting.model.Research;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import com.daimajia.swipe.SimpleSwipeListener;
-import com.daimajia.swipe.SwipeLayout;
-import com.daimajia.swipe.adapters.BaseSwipeAdapter;
 
-public class MeetingData_research_adapter extends BaseSwipeAdapter{
+public class MeetingData_research_adapter extends BaseAdapter{
 	
 	List<Research>mData;
 	private Context mContext;
@@ -55,72 +50,46 @@ public class MeetingData_research_adapter extends BaseSwipeAdapter{
 		// TODO Auto-generated method stub
 		return position;
 	}
-
+	
 	@Override
-	public void fillValues(final int position, View convertView) {
+	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		TextView research_title = (TextView)convertView.findViewById(R.id.research_title);
-		TextView research_intro = (TextView)convertView.findViewById(R.id.research_intro);
-		ImageView deletebtn = (ImageView)convertView.findViewById(R.id.delete);
-		deletebtn.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				if(mOnItemClickListener!= null){
-					mOnItemClickListener.onItemClick(position);
-				}
-				
-			}
-		});
-		research_title.setText(mData.get(position).getTitle());
-		research_intro.setText(mData.get(position).getContent());
+		ViewHolder h;
+		if(convertView == null)
+		{
+			convertView = LayoutInflater.from(mContext).inflate(R.layout.lv_research_item, null);
+			h = new ViewHolder(convertView);
+			convertView.setTag(h);
+		}else
+		{
+			h = (ViewHolder)convertView.getTag();
+		}
+		h.research_title.setText(mData.get(position).getTitle());
+		h.research_intro.setText(mData.get(position).getContent());
+		
+		return convertView;
+	}
+
+	static class ViewHolder {
+		@InjectView(R.id.research_title) TextView research_title;
+		@InjectView(R.id.research_intro) TextView  research_intro;
+		
+		
+		public ViewHolder(View v) {
+			// TODO Auto-generated constructor stub
+			ButterKnife.inject(this, v);
+		}
 		
 	}
 
-	@Override
-	public View generateView(final int position, ViewGroup parent) {
-		// TODO Auto-generated method stub
-		View v = LayoutInflater.from(mContext).inflate(R.layout.lv_research_item, null);
-		SwipeLayout swipeLayout = (SwipeLayout)v.findViewById(getSwipeLayoutResourceId(position));
-		swipeLayout.addSwipeListener(new SimpleSwipeListener(){
-				@Override
-				public void onOpen(SwipeLayout layout) {
-					// TODO Auto-generated method stub
-				}
-				@Override
-				public void onUpdate(SwipeLayout layout, int leftOffset,
-					int topOffset) {
-				// TODO Auto-generated method stub
-					if(mOnUpdateListener != null){
-						mOnUpdateListener.onUpdateListener(position);
-					}
-				super.onUpdate(layout, leftOffset, topOffset);
-				}
-				
-				@Override
-				public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
-				// TODO Auto-generated method stub
-					if(mOnHandleListener!= null){
-						mOnHandleListener.onHandlerListener(position);
-					}
-				super.onHandRelease(layout, xvel, yvel);
-				}
-		});
-		swipeLayout.setOnDoubleClickListener(new SwipeLayout.DoubleClickListener() {
-			
-			@Override
-			public void onDoubleClick(SwipeLayout layout, boolean surface) {
-				// TODO Auto-generated method stub
-			}
-		});
-		return v;
-	} 
 
-	@Override
-	public int getSwipeLayoutResourceId(int position) {
-		// TODO Auto-generated method stub
-		return R.id.swipe4;
-	}
+
+	/**
+	 * research_title.setText(mData.get(position).getTitle());
+		research_intro.setText(mData.get(position).getContent());
+	 lv_research_item
+	 * @param listener
+	 */
 	
 	public void setOnItemClickListener(OnItemClickListener listener){
 		this.mOnItemClickListener = listener;
