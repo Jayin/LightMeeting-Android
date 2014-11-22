5c736a9732173156b993252fd6da1cf02a264451
jason <zyt769562065@163.com>
2014-11-22 13:58:48 +0800

    Doc文档加入解析

--- src/meizhuo/org/lightmeeting/model/Doc.java
+++ src/meizhuo/org/lightmeeting/model/Doc.java
@@ -1,6 +1,55 @@
 package meizhuo.org.lightmeeting.model;
 
-public class Doc {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.google.gson.Gson;
+
+@SuppressWarnings("serial")
+public class Doc implements Serializable{
+	
+	/**
+	 * 解析文档
+	 * @param json
+	 * @return
+	 */
+	public static  Doc create_by_json(String json){
+		try {
+			Gson gson  = new Gson();
+			return (Doc)gson.fromJson(json, Doc.class);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+	}
+	
+	/**
+	 * 解析文档列表 
+	 * @param jsonarray
+	 * @return
+	 */
+	public static List<Doc>create_by_jsonarray(String jsonarray){
+		List<Doc>list =  new ArrayList<Doc>();
+		JSONObject obj = null;
+		JSONArray array = null;
+		try {
+			obj = new JSONObject(jsonarray);
+			array = obj.getJSONArray("response");
+			for(int i=0;i<array.length();i++)
+			{
+				list.add(create_by_json(array.getJSONObject(i).toString()));
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return list;
+	}
+	
 	
 	
 	public String getId() {
